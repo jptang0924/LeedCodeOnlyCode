b823ee1db5e73a11c58c77a8e3b343d0b76c0725
jptang <jptang@tencent.com>
2019-03-05 21:01:48 +0800

    刷题

--- LeedCodeTencentSortAndSearch/KthSmallest.java
+++ LeedCodeTencentSortAndSearch/KthSmallest.java
@@ -1,5 +1,7 @@
 package LeedCodeTencentSortAndSearch;
 
+import java.util.Stack;
+
 /**
  * 二叉搜索树中第K小的元素
  * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
@@ -40,26 +42,43 @@ public class KthSmallest {
 	 
 	 public static int kthSmallest(TreeNode root, int k) {
 		 TreeNode ans = new TreeNode(0);
-		 inOrder(root, k, ans);
+		 inOrderRecursive(root, k, ans);
 		 return ans.val;
 	 }
 	 
 	 //中序遍历
-	 private static int inOrder(TreeNode root, int count, TreeNode ans){
+	 private static int inOrderRecursive(TreeNode root, int count, TreeNode ans){
 		 if(root!=null && count>0){
-			 count = inOrder(root.left, count, ans);
+			 count = inOrderRecursive(root.left, count, ans);
 			 count--;
 			 if(count==0){
 				 ans.val = root.val;
 				 return count;
 			 }
-			 count = inOrder(root.right, count, ans);
+			 count = inOrderRecursive(root.right, count, ans);
 		 }
 		 return count;
 	 }
 	 
 	 //中序遍历迭代
-	 private static void inOrder2(TreeNode root, int count, TreeNode ans){
-		 
+	 private static int inOrderTraversal(TreeNode root, int k){
+         Stack<TreeNode> stack = new Stack<>();
+         TreeNode temp = root;
+	 	 int count = 0;
+	 	 while(temp!=null || !stack.isEmpty()){
+	 	     while (temp!=null){
+	 	         stack.push(temp);
+	 	         temp = temp.left;
+             }
+	 	     if(!stack.isEmpty()){
+                 TreeNode pop = stack.pop();
+                 count++;
+                 if(count==k){
+                     return pop.val;
+                 }
+	 	         temp = pop.right;
+             }
+		 }
+	 	 return 0;
 	 }
 }
