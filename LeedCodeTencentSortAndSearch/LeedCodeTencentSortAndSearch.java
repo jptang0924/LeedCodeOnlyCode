fea5a5be23e2c04443bb2824c39b898efed31b67
jptang <291391649@qq.com>
2019-03-10 16:54:09 +0800

    刷题

--- LeedCodeTencentSortAndSearch/LeedCodeTencentSortAndSearch.java
+++ LeedCodeTencentSortAndSearch/LeedCodeTencentSortAndSearch.java
@@ -228,7 +228,8 @@ public class LeedCodeTencentSortAndSearch {
 	/**
 	 * 二叉树的最近公共祖先
 	 * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
-	 * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+	 * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，
+	 * 满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
 	 * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
 	 * 示例 1:
 	 * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
@@ -245,7 +246,10 @@ public class LeedCodeTencentSortAndSearch {
 	 * @return
 	 */
 	public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
-
+		if(root==q || root==p || root==null)	return root;
+		TreeNode left = lowestCommonAncestor2(root.left, p, q);
+		TreeNode right = lowestCommonAncestor2(root.right, p, q);
+		return left==null ? right : (right==null ? left : root);
 	}
 
 }
